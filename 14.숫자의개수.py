8e283e280c1badcccddfea67ab2808b49e835a5a
WanManDu <wangyu7958@gmail.com>
2024-08-09 15:14:06 +0900

    modified:   "14.\354\210\253\354\236\220\354\235\230\352\260\234\354\210\230.py"

--- 14.숫자의개수.py
+++ 14.숫자의개수.py
@@ -4,8 +4,8 @@ C = int(input())
 
 mul = A * B * C
 mul = str(mul)
-mul_temp = [0]*10
+mul_num = [0]*10
 for i in mul:
-    mul_temp[int(i)] += 1
-for i in mul_temp:
+    mul_num[int(i)] += 1
+for i in mul_num:
     print(i)
